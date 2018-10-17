9020e689df816f460763eaa7001c472a0850573d
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-10-17 16:47:03 -0400

    Updating

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PG_SECRET)}@localhost/winemaster`, {
+const sequelize = new Sequelize(`postgresql://postgres:${encodeURIComponent(process.env.PG_SECRET)}@localhost/winemaster`, {
     dialect: 'postgres'
 })
 
